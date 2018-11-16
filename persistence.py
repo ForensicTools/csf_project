593d60a80140360a34e5dbacfdf35f4182794117
will <wjm5004@rit.edu>
2018-11-16 13:37:30 -0500

    cleanup, run as sudo, and requirements (requests)

--- persistence.py
+++ persistence.py
@@ -1,3 +1,14 @@
+"""
+Authors:
+Will McDonnell
+Michael Milkovich
+
+persistence.py
+
+Aid investigators by providing an easy to read report of potential
+signs of persistence on a linux system.
+"""
+
 import os
 import stat
 import time
@@ -13,8 +24,10 @@ from crontab import CronTab
 
 def get_bash_info():
     zip_file = ZipFile('user_bash.zip', 'w')
+    # Get system wide bashrc
     if os.path.exists('/etc/bash.bashrc'):
         zip_file.write('/etc/bash.bashrc', arcname='bash.bashrc')
+    # All users
     for p in pwd.getpwall():
         user = User(p)
         if os.path.exists(user.dir + '/.bash_profile'):
@@ -33,6 +46,7 @@ def get_chrome_extensions(user):
             for ext in extensions:
                 if ext == 'Temp':
                     continue
+                # All extension IDs can be resolved with the below URL, otherwise output ID
                 a = requests.get('https://chrome.google.com/webstore/detail/{}'.format(ext), timeout=2)
                 if a.status_code == 200:
                     e = re.search(r'<title[^>]*>([^<]+)</title>', a.content.decode())
@@ -54,6 +68,7 @@ class Entry:
         self.error = False
         self.hidden = os.path.basename(path).startswith('.')
         try:
+            # Device files and other weird files have issues with stat, can be ignored
             path_stat = os.lstat(path)
         except Exception as e:
             print('error: {}'.format(e))
@@ -63,8 +78,6 @@ class Entry:
         self.access_time = time.ctime(path_stat.st_atime)
         self.mod_time = time.ctime(path_stat.st_mtime)
         self.meta_time = time.ctime(path_stat.st_ctime)
-        self.owner = pwd.getpwuid(path_stat.st_uid)[0]
-        self.group = grp.getgrgid(path_stat.st_gid)[0]
         self.is_exec = os.access(path, os.X_OK)
 
         if self.hidden and self.is_exec:
@@ -81,8 +94,8 @@ class Root:
         self.entries = []
         for root, dirs, files in os.walk(path, topdown=True):
             dirs[:] = [d for d in dirs if d not in ignore_dir]
-            for dir in dirs:
-                new_dir = Entry(os.path.join(root, dir), hidden_entries, uid_entries, gid_entries, ignore_dir)
+            for directory in dirs:
+                new_dir = Entry(os.path.join(root, directory), hidden_entries, uid_entries, gid_entries, ignore_dir)
                 del new_dir
             for file in files:
                 new_file = Entry(os.path.join(root, file), hidden_entries, uid_entries, gid_entries, ignore_dir)
@@ -109,12 +122,15 @@ class User:
 
 
 if __name__ == "__main__":
+    if os.getuid() != 0:
+        print("Error: Insufficient permissions, run with sudo")
+        exit(1)
     report = open('report.txt', 'w')
     hidden_entries = []
     set_uid_entries = []
     set_gid_entries = []
     ignore_directory = ['/proc', '/sys/bus', '/sys/dev', '/sys/devices', '/sys/block', '/sys/class', '/sys/module']
-    root = Root('/', hidden_entries, set_uid_entries, set_gid_entries, ignore_directory)
+    Root('/', hidden_entries, set_uid_entries, set_gid_entries, ignore_directory)
 
     std = sorted(hidden_entries, key=lambda file: (os.path.dirname(file.path), os.path.basename(file.path)))
     report.write('===========Hidden Executables===========\n')
@@ -166,6 +182,6 @@ if __name__ == "__main__":
     report.write('\n')
     report.write('==========Active Users==========\n')
     report.write('user, terminal, pid, start_time\n')
-    for user in current_users:
-        report.write('{}, {}, {}, {}\n'.format(user.name, user.terminal, user.pid, time.ctime(user.started)))
+    for active_user in current_users:
+        report.write('{}, {}, {}, {}\n'.format(active_user.name, active_user.terminal, active_user.pid, time.ctime(active_user.started)))
     report.close()
